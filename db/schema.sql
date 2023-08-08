c1e653ef9addb8213e0711ad32b5cce212455006
Brayden Day <braydenday99@gmail.com>
2023-08-07 19:06:40 -0600

    created database for department, role and employee

---
+++ db/schema.sql
@@ -0,0 +1,35 @@
+drop database if exists work_db;
+
+create database work_db;
+
+use work_db;
+
+create table department (
+    id int not null auto_increment primary key,
+    name varchar(30) not null
+);
+
+create table role (
+    id int not null auto_increment primary key,
+    title varchar(30) not null,
+    department_id int,
+    salary decimal,
+    foreign key (department_id)
+    references department(id)
+    on delete cascade
+);
+
+create table employee (
+    id int not null auto_increment primary key,
+    first_name varchar(30) not null,
+    last_name varchar(30) not null,
+    role_id int,
+    manager_id int,
+    foreign key (role_id)
+    references role(id),
+    foreign key (manager_id)
+    references employee(id)
+    on delete set null
+);
+
+
